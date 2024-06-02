155c754fe3322c4f37af4a08fc3726b6c498de65
Pranav R <pranav81234@gmail.com>
2024-06-02 12:14:39 +0530

    Initial Commit

---
+++ Tasker/TaskerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by Pranav R on 02/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
