acc2cf1577b1c448ea053bc238c88eec205accb5
Pranav R <pranav81234@gmail.com>
2024-06-03 13:19:50 +0530

    Task View Created

---
+++ Tasker/TaskItem.swift
@@ -0,0 +1,18 @@
+//
+//  TaskItem.swift
+//  Tasker
+//
+//  Created by Pranav R on 03/06/24.
+//
+
+import SwiftUI
+
+struct TaskItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskItem()
+}
